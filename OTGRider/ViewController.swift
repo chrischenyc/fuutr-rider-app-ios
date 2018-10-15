195e8d416391436e18900bfcaed96d054525d8a8
Chris Chen <chrischen79@gmail.com>
2018-10-15 15:40:39 +0800

    Initial Commit

---
+++ OTGRider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTGRider
+//
+//  Created by Chris Chen on 15/10/18.
+//  Copyright © 2018 OTGRide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
