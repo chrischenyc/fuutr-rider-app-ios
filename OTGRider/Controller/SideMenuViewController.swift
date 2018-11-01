c7dd904b363d69231174a88985c8915502cd48e1
Chris Chen <chrischen79@gmail.com>
2018-11-01 17:41:42 +1100

    Settings: display vesion

--- OTGRider/Controller/SideMenuViewController.swift
+++ OTGRider/Controller/SideMenuViewController.swift
@@ -20,8 +20,16 @@ class SideMenuViewController: UITableViewController {
     
     var selectedMenuItem: SideMenuItem?
     
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let appName = Bundle.main.infoDictionary!["CFBundleName"] as? String,
+            let appVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as? String {
+            versionLabel.text = "\(appName) v\(appVersion)"
+        }
+        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
