3d9e2a8cbc6d638665112e85367578e863fec572
Chris Chen <chrischen79@gmail.com>
2018-11-06 20:52:02 +1100

    Storyboard: add email sign up and log in scenes

---
+++ OTGRider/Controller/EmailSignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EmailSignUpViewController.swift
+//  OTGRider
+//
+//  Created by Chris Chen on 6/11/18.
+//  Copyright © 2018 OTGRide. All rights reserved.
+//
+
+import UIKit
+
+class EmailSignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func unwindToEmailSignUp(_ unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+}
