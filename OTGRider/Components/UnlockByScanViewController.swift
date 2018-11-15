36c8119389866958496a1c162369675c4decaade
Chris Chen <chrischen79@gmail.com>
2018-11-15 18:31:54 +1100

    Scan: toggle torch

--- OTGRider/Components/UnlockByScanViewController.swift
+++ OTGRider/Components/UnlockByScanViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class UnlockByScanViewController: UIViewController {
-
+    
+    @IBOutlet weak var torchButton: UIButton!
+    private var torchOn:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        torchButton.setTitle("Torch On", for: .normal)
     }
     
-
+    
     @IBAction func unwindToUnlockByScan(_ unwindSegue: UIStoryboardSegue) {
         // let sourceViewController = unwindSegue.source
         // Use data from the view controller which initiated the unwind segue
     }
-
+    
+    @IBAction func torchButtonTapped(_ sender: Any) {
+        torchOn = !torchOn
+        toggleTorch(on: torchOn)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        torchOn = false
+        toggleTorch(on: torchOn)
+    }
+    
+    private func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else{ return }
+        
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+                
+                if (on) {
+                    device.torchMode = .on
+                    torchButton.setTitle("Torch Off", for: .normal)
+                    
+                } else {
+                    device.torchMode = .off
+                    torchButton.setTitle("Torch On", for: .normal)
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                logger.error("Torch could not be used")
+            }
+        }
+        else{
+            logger.error("Torch is not available")
+        }
+    }
 }
