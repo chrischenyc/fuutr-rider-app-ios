39e7b9d6aeb86a61a82e8aa54b8d374e025804a7
jackylimel <veryfaint@gmail.com>
2019-01-28 17:15:00 +1100

    drawing ride path on RideFinishedViewController

--- OTGRider/Components/RideFinishedViewController.swift
+++ OTGRider/Components/RideFinishedViewController.swift
@@ -10,6 +10,9 @@ class RideFinishedViewController: UIViewController {
   @IBOutlet weak var reportButton: UIButton!
   @IBOutlet weak var continueButton: UIButton!
   
+  private let streetZoomLevel: Float = 14.0
+  private var ongoingRidePolyline: GMSPolyline?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     continueButton.primaryRed()
@@ -20,13 +23,41 @@ class RideFinishedViewController: UIViewController {
     rideFinishedTimeLabel.textColor = UIColor.primaryDarkColor
     
     continueButton.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
+    
+    do {
+      // Set the map style by passing the URL of the local file.
+      if let styleURL = Bundle.main.url(forResource: "GoogleMapStyle", withExtension: "json") {
+        mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+      } else {
+        logger.error("Unable to find style.json")
+      }
+    } catch {
+      logger.error("One or more of the map styles failed to load. \(error)")
+    }
   }
   
-  func updateContent(with ride: Ride) {
+  func updateContent(with ride: Ride, and location: CLLocation?) {
     costLabel.text = ride.totalCost.currencyString
     rideUsedTimeLabel.text = ride.duration.hhmmssString
     rideDistanceLabel.text = ride.distance.distanceString
     rideFinishedTimeLabel.text = ride.lockTime?.dateTimeString
+    
+    if let location = location {
+      let camera = GMSCameraPosition.camera(withTarget: location.coordinate, zoom: streetZoomLevel)
+      mapView.animate(to: camera)
+    }
+    
+    if let encodedPath = ride.encodedPath, let decodedPath = GMSMutablePath(fromEncodedPath: encodedPath){
+      
+      drawRoute(forPath: decodedPath)
+    }
+  }
+  
+  private func drawRoute(forPath path: GMSPath?) {
+    ongoingRidePolyline = GMSPolyline(path: path)
+    ongoingRidePolyline?.strokeWidth = 4
+    ongoingRidePolyline?.strokeColor = UIColor.primaryRedColor
+    ongoingRidePolyline?.map = mapView
   }
   
   @objc private func continueButtonTapped() {
