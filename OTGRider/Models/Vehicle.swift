494fe13d7daa1d8d7616868d82aee578d2bec3e2
Chris Chen <chrischen79@gmail.com>
2019-01-28 13:34:46 +1100

    Update Vehicle model to include 2 new fields "locked" and "inRide"

--- OTGRider/Models/Vehicle.swift
+++ OTGRider/Models/Vehicle.swift
@@ -10,39 +10,43 @@ import Foundation
 import ObjectMapper
 
 struct Vehicle: Mappable {
-    var _id: String = ""
-    var powerPercent: Int?
-    var remainderRange: Double?
-    var latitude: Double?
-    var longitude: Double?
-    var vehicleCode: String?
-    var reserved: Bool = false
-    var reservedUntil: Date?
-    var canReserveAfter: Date?
-    var unlockCost: Double = 0
-    var rideMinuteCost: Double = 0
-    var pauseMinuteCost: Double = 0
+  var _id: String = ""
+  var powerPercent: Int?
+  var remainderRange: Double?
+  var latitude: Double?
+  var longitude: Double?
+  var vehicleCode: String?
+  var reserved: Bool = false
+  var reservedUntil: Date?
+  var canReserveAfter: Date?
+  var unlockCost: Double = 0
+  var rideMinuteCost: Double = 0
+  var pauseMinuteCost: Double = 0
+  var locked: Bool = false  // vehicle may be locked if a ride is paused
+  var inRide: Bool = false
+  
+  init?(map: Map) {
     
-    init?(map: Map) {
-        
-    }
-    
-    mutating func mapping(map: Map) {
-        _id             <- map["_id"]
-        powerPercent    <- map["powerPercent"]
-        remainderRange  <- map["remainderRange"]
-        latitude        <- map["latitude"]
-        longitude       <- map["longitude"]
-        vehicleCode     <- map["vehicleCode"]
-        reserved        <- map["reserved"]
-        reservedUntil   <- (map["reservedUntil"], CustomDateFormatTransform(formatString: "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
-        canReserveAfter <- (map["canReserveAfter"], CustomDateFormatTransform(formatString: "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
-        unlockCost      <- map["unlockCost"]
-        rideMinuteCost  <- map["rideMinuteCost"]
-        pauseMinuteCost <- map["pauseMinuteCost"]
-    }
-    
-    static func fromJSONArray(_ jsonArray: [JSON]) -> [Vehicle]? {
-        return Mapper<Vehicle>().mapArray(JSONArray: jsonArray)
-    }
+  }
+  
+  mutating func mapping(map: Map) {
+    _id             <- map["_id"]
+    powerPercent    <- map["powerPercent"]
+    remainderRange  <- map["remainderRange"]
+    latitude        <- map["latitude"]
+    longitude       <- map["longitude"]
+    vehicleCode     <- map["vehicleCode"]
+    reserved        <- map["reserved"]
+    reservedUntil   <- (map["reservedUntil"], CustomDateFormatTransform(formatString: "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
+    canReserveAfter <- (map["canReserveAfter"], CustomDateFormatTransform(formatString: "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
+    unlockCost      <- map["unlockCost"]
+    rideMinuteCost  <- map["rideMinuteCost"]
+    pauseMinuteCost <- map["pauseMinuteCost"]
+    locked          <- map["locked"]
+    inRide          <- map["inRide"]
+  }
+  
+  static func fromJSONArray(_ jsonArray: [JSON]) -> [Vehicle]? {
+    return Mapper<Vehicle>().mapArray(JSONArray: jsonArray)
+  }
 }
