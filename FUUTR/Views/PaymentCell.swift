8e565c7113085e8dc8bc0caa82fa7f98dfa33869
Chris Chen <chrischen79@gmail.com>
2019-02-13 20:01:33 +1100

    payment history

--- FUUTR/Views/PaymentCell.swift
+++ FUUTR/Views/PaymentCell.swift
@@ -12,21 +12,10 @@ class PaymentCell: UITableViewCell {
   
   @IBOutlet weak var amountLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
-  @IBOutlet weak var descriptionLabel: UILabel!
   
   func loadPayment(_ payment: Payment) {
     amountLabel.text = payment.amount?.priceString
-    
-    dateLabel.text = payment.date?.dateTimeString ?? ""
-    
-    var paymentDescription = ""
-    if let description = payment.description {
-      paymentDescription += description
-    }
-    if let lastFour = payment.lastFour {
-      paymentDescription += " paid with \(lastFour)"
-    }
-    descriptionLabel.text = paymentDescription
+    dateLabel.text = payment.date?.dateTimeString
   }
   
 }
